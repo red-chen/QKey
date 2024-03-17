c7918de1b7bfaacbc3e87aa785fe136fd8a4ccf7
Charles Chen <redchen1255@gmail.com>
2024-03-17 16:18:32 +0800

    init app.

---
+++ QKey/QKey/QKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QKeyApp.swift
+//  QKey
+//
+//  Created by liushiyu on 2024/3/17.
+//
+
+import SwiftUI
+
+@main
+struct QKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
